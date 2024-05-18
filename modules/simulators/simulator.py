d4da874e9e9a2dc32fbfd56db3e2b19eecbcef55
VicenteMoraes <vicenteromeiromoraes@gmail.com>
2024-05-18 19:20:23 -0300

    Updated issue with headless simulation

--- modules/simulators/simulator.py
+++ modules/simulators/simulator.py
@@ -5,7 +5,8 @@ from docker import DockerClient
 class Simulator(DockerModule):
     def __init__(self, docker_client: DockerClient, path: str, headless_command: str, gui_command: str, headless: bool = True,
                  tag: str = "sim", path_to_world: str = "", world_arg: str = "world_map", *args, **kwargs):
-        super(Simulator, self).__init__(docker_client=docker_client, path=path, tag=tag, command="", *args, **kwargs)
+        super(Simulator, self).__init__(docker_client=docker_client, path=path, tag=tag, command="", headless=headless,
+                                        *args, **kwargs)
         self.headless = headless
         self.env['RMW_IMPLEMENTATION'] = 'rmw_cyclonedds_cpp'
 
--- tests/robots/test_turtlebot3_nav2.py
+++ tests/robots/test_turtlebot3_nav2.py
@@ -7,18 +7,17 @@ from core import pose
 
 
 def test_turtlebot3withnav2():
-    return
     client = docker.from_env()
     network = ROS2Network(client, name="ros2")
     sim = Gazebo(client, headless=False, auto_remove=True, network=network, path_to_world="/opt/ros/humble/share/turtlebot3_gazebo/worlds/turtlebot3_world.world")
-    sim.add(DockerLogger(target='', write_to_file=True, filename='sim.log', timeout=300, update_interval=1))
+    sim.add_logger(write_to_file=True, filename="sim.log")
     ps = pose.Pose()
     ps.position.x = -2
     ps.position.y = -0.5
     ps.position.z = 0.1
     robot = Turtlebot3withNav2(client, robot_name="turtlebot", robot_namespace="turtlebot", auto_remove=True, network=network,
                                initial_pose=ps, use_rviz=True)
-    robot.add(DockerLogger(target='', write_to_file=True, filename='robot.log', timeout=300))
+    robot.add_logger(target='', write_to_file=True, filename='robot.log', timeout=300)
     sim.add_model_path(container=robot, path="/opt/ros/humble/share/turtlebot3_gazebo")
     ps = pose.Pose()
     ps.position.x = 2
@@ -26,7 +25,7 @@ def test_turtlebot3withnav2():
     ps.position.z = 0.1
     robot2 = Turtlebot3withNav2(client, robot_name="turtlebot2", robot_namespace="turtlebot2", container_name="turtlebot2",
                                 auto_remove=True, network=network, initial_pose=ps, use_rviz=True)
-    robot2.add(DockerLogger(target='', write_to_file=True, filename='robot2.log', timeout=300))
+    robot2.add_logger(target='', write_to_file=True, filename='robot2.log', timeout=300)
 
     network.build()
     sim.build()
