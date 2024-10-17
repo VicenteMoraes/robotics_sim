a9b1d121f7b94b06d2814b198ee4b3b89a77bc2c
VicenteMoraes <vicenteromeiromoraes@gmail.com>
2024-10-17 10:10:30 -0300

    Changes to be committed: 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='robotics_sim',
     version='',
     packages=['core', 'tests', 'tests.core', 'tests.ros2', 'tests.robots', 'tests.loggers', 'tests.simulators',
-              'trials', 'plugins'],
+              'trials', 'modules'],
     url='https://github.com/VicenteMoraes/robotics_sim',
     license='',
     author='VicenteMoraes',
